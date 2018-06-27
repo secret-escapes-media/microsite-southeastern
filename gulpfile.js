870e424dd4b6c83b469bd77eef9f1f359b9656ed
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-06-27 13:53:20 +0100

    Merge branch 'master' into phase-two

--- gulpfile.js
+++ gulpfile.js
@@ -118,7 +118,7 @@ gulp.task('build-main-js', function(cb) {
     './_assets/js/_components/standard.js',
     './_assets/js/_components/offer-countdown.js',
     './_assets/js/_components/modal.js',
-    './_assets/js/_components/modal-nav.js',
+    // './_assets/js/_components/modal-nav.js',
     // './_assets/js/_components/sticky-nav.js',
 
     // custom js for project
